499802e6ffc97c3bd7ef91917f527534585f2369
MadeByJax <jaxzynfollett@outlook.com>
2023-10-08 15:08:06 +1100

    add: function to close dropdown

---
+++ src/components/ClickOutsideHandler.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useRef } from "react";
+
+const ClickOutsideHandler = ({ onClickOutside, children }) => {
+  const wrapperRef = useRef(null);
+
+  const handleClickOutside = (event) => {
+    if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+      onClickOutside();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [onClickOutside]);
+
+  return <div ref={wrapperRef}>{children}</div>;
+};
+
+export default ClickOutsideHandler;
